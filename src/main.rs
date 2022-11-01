51e476efe4ade1d954fa1c5ace9a8f509bbdfefa
Matthew J. Berger <matthewberger@nevada.unr.edu>
2022-10-31 18:40:39 -0600

    Celebrate the doggys birthday

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
-struct Dog;
+struct Dog {
+    age: u8,
+}
+
+impl Dog {
+    pub fn celebrate_birthday(&mut self) {
+        self.age = self.age + 1;
+        println!("Wiggly butt is {} wags old!", self.age);
+    }
+}
 
 fn main() {
-    let _dog = Dog {};
+    let mut dog = Dog { age: 8 };
+    dog.celebrate_birthday();
 }
