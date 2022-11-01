8f6990366c64b7e39e164b90af50977fffb0278a
Matthew J. Berger <matthewberger@nevada.unr.edu>
2022-10-31 19:00:30 -0600

    Give the dog a bone

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
 pub type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
 #[derive(Debug)]
+struct AnimalError {
+    details: String,
+}
+
+impl AnimalError {
+    fn new(msg: &str) -> Self {
+        Self {
+            details: msg.to_string(),
+        }
+    }
+}
+
+impl std::error::Error for AnimalError {
+    fn description(&self) -> &str {
+        &self.details
+    }
+}
+
+impl std::fmt::Display for AnimalError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.details)
+    }
+}
+
+#[derive(Debug)]
 struct Dog {
     age: u8,
     bone: Option<Bone>, // The dog may or may not be holding a Bone
@@ -25,8 +50,30 @@ impl Dog {
         println!("Wiggly butt is {} wags old!", self.age);
     }
 
-    pub fn speak(&self) {
-        println!("Woof!");
+    pub fn speak(&self) -> Result<()> {
+        match self.bone.as_ref() {
+            Some(bone) => Err(Box::new(AnimalError::new(&format!(
+                "Dog can't speak because of the {:?} bone!",
+                bone
+            )))),
+            None => Ok(println!("Woof!")),
+        }
+    }
+
+    fn receive_bone(&mut self, bone: Bone) -> Result<()> {
+        match self.bone.as_ref() {
+            Some(bone) => {
+                return Err(Box::new(AnimalError::new(&format!(
+                    "Dog already has a bone! ({:?})",
+                    bone
+                ))))
+            }
+            None => {
+                println!("Doggy grabbed the {:?} bone!", bone.kind);
+                self.bone = Some(bone);
+            }
+        };
+        Ok(())
     }
 }
 
@@ -41,6 +88,12 @@ impl Bone {
     }
 }
 
+impl From<BoneKind> for Bone {
+    fn from(kind: BoneKind) -> Self {
+        Self { kind }
+    }
+}
+
 #[derive(Debug)]
 enum BoneKind {
     BaconFlavored,
@@ -51,6 +104,15 @@ enum BoneKind {
 fn main() -> Result<()> {
     let mut dog = Dog::new(8);
     dog.celebrate_birthday();
-    dog.speak();
+    dog.speak()?;
+
+    let bone = Bone::from(BoneKind::BaconFlavored);
+    dog.receive_bone(bone)?;
+
+    // Uncomment this to give the dog a bone while he already has one!
+    //
+    // let bone = Bone::from(BoneKind::BaconFlavored);
+    // dog.receive_bone(bone)?;
+
     Ok(())
 }
